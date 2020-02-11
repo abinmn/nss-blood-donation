d0346a2d0a1f4a6cab9c3fe8a1ec8e7759c9a300
Abin Mn <abinmn619@gmail.com>
2020-02-12 00:33:37 +0530

    Add all models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from app.models import District
+from app.models import Taluk
+from app.models import College
+from app.models import BloodGroup
+from app.models import Student
+from app.models import Donation
+
+
+admin.site.register(District)
+admin.site.register(Taluk)
+admin.site.register(College)
+admin.site.register(BloodGroup)
+admin.site.register(Student)
+admin.site.register(Donation)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
-# Create your models here.
+
+class District(models.Model):
+    """Table to store District details."""
+
+    name = models.CharField(max_length=200)
+
+
+class Taluk(models.Model):
+    """Table to store District details."""
+
+    name = models.CharField(max_length=200)
+    district = models.ForeignKey(
+        District,
+        on_delete=models.CASCADE,
+        related_name="taluks")
+
+
+class College(models.Model):
+    """Table to store District details."""
+
+    name = models.CharField(max_length=200)
+    taluk = models.ForeignKey(
+        Taluk,
+        on_delete=models.CASCADE,
+        related_name="colleges")
+
+
+class BloodGroup(models.Model):
+    """Table to store blood group details."""
+
+    group = models.CharField(max_length=10)
+
+
+class Student(models.Model):
+    """Table to store student details."""
+
+    name = models.CharField(max_length=300)
+    student_class = models.CharField(max_length=10)
+    mobile_number = models.BigIntegerField()
+    email = models.EmailField()
+    passout_year = models.IntegerField()
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    blood_group = models.ForeignKey(
+        BloodGroup,
+        on_delete=models.CASCADE,
+        related_name="members")
+    college = models.ForeignKey(
+        College,
+        on_delete=models.CASCADE,
+        related_name="students"
+    )
+
+
+class Donation(models.Model):
+    """Table to store District details."""
+
+    student = models.ForeignKey(
+        Student,
+        on_delete=models.CASCADE,
+        related_name="donation_history")
+    donated_date = models.DateField(auto_now=True)
