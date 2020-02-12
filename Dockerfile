52011b362bde5295421fbb65e2d1948d329bb68b
Abin Mn <abinmn619@gmail.com>
2020-02-13 02:00:14 +0530

    Add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.7
+RUN pip install pipenv
+COPY . /app
+WORKDIR /app
+
+RUN pipenv lock --requirements > requirements.txt
+RUN pip install -r requirements.txt
---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Collect static files
+echo "Makemigrations"
+python manage.py makemigrations
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+# Start server
+echo "Starting server"
+python manage.py runserver 0.0.0.0:80
